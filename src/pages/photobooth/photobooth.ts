fa8dd0d5ca52ca5d5de786a93d5da41f464a5605
Yehoshoua <josue@tocndix.com>
2019-10-23 16:54:32 +0300

    remove bug

--- src/pages/photobooth/photobooth.ts
+++ src/pages/photobooth/photobooth.ts
@@ -189,7 +189,7 @@ export class PhotoboothPage {
     this.barcodeScanner.scan().then(barcodeData => {
       if(barcodeData.cancelled == false){
         this.scannedCode = barcodeData.text;
-        var tel  = this.scannedCode.substring(0, this.scannedCode.length - localStorage.getItem("idEvent").length);
+        var tel  = this.scannedCode.substring(0, this.scannedCode.length - 1);
         tel = tel.replace('https://qrcode.tag.by/vcard?tag=', '');
         console.log('here 1');
         this.selectEntryByQR(tel);
